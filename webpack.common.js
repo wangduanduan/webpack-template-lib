c85a9d57bb5db3c3aa40fd34dcd279f96a6ef1a0
wangduanduan <1779706607@qq.com>
2018-12-03 15:43:46 +0800

    updare dependence package

--- webpack.common.js
+++ webpack.common.js
@@ -33,7 +33,15 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       {
         test: /\.(html)$/,
         use: {
